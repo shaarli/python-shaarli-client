f982850e05e08a20e4637f585a360a2659947771
nodiscc <nodiscc@gmail.com>
2020-01-08 18:37:41 +0100

    shaarli_client: bump version to 0.4.0

--- shaarli_client/__init__.py
+++ shaarli_client/__init__.py
@@ -1,5 +1,5 @@
 """shaarli-client"""
 __title__ = 'shaarli_client'
 __brief__ = 'CLI to interact with a Shaarli instance'
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 __author__ = 'The Shaarli Community'
